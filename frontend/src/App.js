163252a559a09fbcec763cecf9611f25659e27d4
Sunidhi Tiwari <tiwarisun10@gmail.com>
2024-03-27 16:48:09 +0530

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+// App.js
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import KeyFeatures from "./components/KeyFeatures";
+import FAQ from "./components/FAQ";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+// Import other components as needed
 
 function App() {
-  return <div>Project Collab</div>;
+  return <div className="App"></div>;
 }
 
 export default App;
