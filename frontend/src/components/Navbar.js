ac97c507460fe4d9f70c7f62419c9183641dd638
Sunidhi Tiwari <tiwarisun10@gmail.com>
2024-04-14 21:35:25 +0530

    UI

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,44 +1,94 @@
-import React from 'react'
-import { Link, useLocation, useNavigate} from "react-router-dom";
-
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Navbar = (props) => {
-    let location = useLocation();
-    let navigate = useNavigate();
-    
-    const handleLogout=()=>{
-      localStorage.removeItem('token');
-      navigate('/login');
-      props.showAlert("Logged out succesfully","success")
-    }
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Project Collab Hub</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item mx-2">
-                            <Link className={`nav-link ${location.pathname==="/"? "active": ""}`} aria-current="page" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link className={`nav-link ${location.pathname==="/about"? "active": ""}`} to="/about">About</Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                            <Link className={`nav-link ${location.pathname==="/addProject"? "active": ""}`} to="/addProject">Add Project</Link>
-                        </li>
-                    </ul>
-                    {!localStorage.getItem('token')?
-                    <form className="d-flex">            
-                      <Link className="btn btn-primary mx-2" role="button" to='/login'>Login</Link>
-                      <Link className="btn btn-primary mx-2" role="button" to='/signup'>Signup</Link>
-                    </form>:<button className="btn btn-primary" onClick={handleLogout}>Logout</button> }
-                </div>
-            </div>
-        </nav>
-    )
-}
+  let location = useLocation();
+  let navigate = useNavigate();
 
-export default Navbar
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+    props.showAlert("Logged out succesfully", "success");
+  };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Project Collab Hub
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item mx-2">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/" ? "active" : ""
+                }`}
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/about" ? "active" : ""
+                }`}
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/addProject" ? "active" : ""
+                }`}
+                to="/addProject"
+              >
+                Add Project
+              </Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link
+                className={`nav-link ${
+                  location.pathname === "/user/:activepage" ? "active" : ""
+                }`}
+                to="/user/:activepage"
+              >
+                Your Profile
+              </Link>
+            </li>
+          </ul>
+          {!localStorage.getItem("token") ? (
+            <form className="d-flex">
+              <Link className="btn btn-primary mx-2" role="button" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-primary mx-2" role="button" to="/signup">
+                Signup
+              </Link>
+            </form>
+          ) : (
+            <button className="btn btn-primary" onClick={handleLogout}>
+              Logout
+            </button>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
